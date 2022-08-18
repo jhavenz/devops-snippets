4ff086118946b907e0e3fb00f1a8e4d403601126
Jonathan Havens <jhavens@rcnmail.com>
2022-08-18 09:16:24 -0700

    initial commit

---
+++ docker-prune.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# Accreditation
+# https://github.com/gotzmann/snippets/blob/master/docker-prune.sh
+
+# Prune docker system and remove all containers, images, volumes with one command.
+
+docker system prune
+docker container stop $(docker container ls -aq)
+docker container rm $(docker container ls -aq)
+docker rmi $(docker images -aq)
+docker volume prune
